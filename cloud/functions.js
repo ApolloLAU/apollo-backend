df91f467ca95862df724d6c597fd0c7b56425603
peter <39004027+plsakr@users.noreply.github.com>
2021-12-04 17:42:43 +0200

    fixing ml prediction function

--- cloud/functions.js
+++ cloud/functions.js
@@ -37,27 +37,29 @@ Parse.Cloud.beforeSave('SensorData', async (request) => {
   const obj = request.object;
   const all_data = obj.get('ECG');
   const new_data = obj.get('raw_ECG');
-  const req_url = process.env.ML_API_URL + '/preprocess_ecg'
+  if (new_data.length > 0) {
+    const req_url = process.env.ML_API_URL + '/preprocess_ecg'
 
-  const request_obj = {ECG: new_data}
+    const request_obj = {ECG: new_data}
 
-  const response = await axios.post(req_url, request_obj).catch((err) => {
-    console.log('could not process ecg')
-    console.log(err);
-  })
-  // response contains array with same length as input and new last index.
+    const response = await axios.post(req_url, request_obj).catch((err) => {
+      console.log('could not process ecg')
+      console.log(err);
+    })
+    // response contains array with same length as input and new last index.
 
-  // overwrite all data array with the cleaned data.
-  // Array.prototype.splice.apply(all_data, [last_index + 1, response.clean_data.length].concat(response.clean_data))
-  console.log('Cleaned ECG. Response is:', response)
-  const response_object = JSON.parse(response.data.replace(/\bNaN\b/g, -1))
-  if (response_object.statusCode === 200) {
-    all_data.push(...response_object.data.clean_ecg)
-    request.object.set('ECG', all_data);
-    const current_bpm = request.object.get('bpm');
-    current_bpm.push(response_object.data.bpm);
-    request.object.set('bpm', current_bpm);
-    request.object.set('raw_ECG', []);
+    // overwrite all data array with the cleaned data.
+    // Array.prototype.splice.apply(all_data, [last_index + 1, response.clean_data.length].concat(response.clean_data))
+    console.log('Cleaned ECG. Response is:', response)
+    const response_object = JSON.parse(response.data.replace(/\bNaN\b/g, -1))
+    if (response_object.statusCode === 200) {
+      all_data.push(...response_object.data.clean_ecg)
+      request.object.set('ECG', all_data);
+      const current_bpm = request.object.get('bpm');
+      current_bpm.push(response_object.data.bpm);
+      request.object.set('bpm', current_bpm);
+      request.object.set('raw_ECG', []);
+    }
   }
 })
 

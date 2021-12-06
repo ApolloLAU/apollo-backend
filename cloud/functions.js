cfd8f893babdefcb1b3da078144243e43da3d6cd
peter <39004027+plsakr@users.noreply.github.com>
2021-12-06 21:20:13 +0200

    fixing ml prediction function

--- cloud/functions.js
+++ cloud/functions.js
@@ -33,42 +33,42 @@ Parse.Cloud.define('getActiveMissions', req => {
 //
 // });
 
-Parse.Cloud.beforeSave('SensorData', async (request) => {
-  const obj = request.object;
-  const all_data = obj.get('ECG');
-  const new_data = obj.get('raw_ECG');
-  if (new_data.length > 0) {
-    const req_url = process.env.ML_API_URL + '/preprocess_ecg'
-
-    const request_obj = {ECG: new_data}
-
-    const response = await axios.post(req_url, request_obj).catch((err) => {
-      console.log('could not process ecg')
-      console.log(err);
-    })
-    // response contains array with same length as input and new last index.
-
-    // overwrite all data array with the cleaned data.
-    // Array.prototype.splice.apply(all_data, [last_index + 1, response.clean_data.length].concat(response.clean_data))
-    let response_object = {}
-    if (typeof response.data === "string") {
-      console.log('Cleaned ECG. Response is:', response.data)
-      response_object = JSON.parse(response.data.replace(/\bNaN\b/g, -1))
-    } else {
-      response_object = response.data
-    }
-
-
-    if (response_object.statusCode === 200) {
-      all_data.push(...response_object.data.clean_ecg)
-      request.object.set('ECG', all_data);
-      const current_bpm = request.object.get('bpm');
-      current_bpm.push(response_object.data.bpm);
-      request.object.set('bpm', current_bpm);
-      request.object.set('raw_ECG', []);
-    }
-  }
-})
+// Parse.Cloud.beforeSave('SensorData', async (request) => {
+//   const obj = request.object;
+//   const all_data = obj.get('ECG');
+//   const new_data = obj.get('raw_ECG');
+//   if (new_data.length > 0) {
+//     const req_url = process.env.ML_API_URL + '/preprocess_ecg'
+//
+//     const request_obj = {ECG: new_data}
+//
+//     const response = await axios.post(req_url, request_obj).catch((err) => {
+//       console.log('could not process ecg')
+//       console.log(err);
+//     })
+//     // response contains array with same length as input and new last index.
+//
+//     // overwrite all data array with the cleaned data.
+//     // Array.prototype.splice.apply(all_data, [last_index + 1, response.clean_data.length].concat(response.clean_data))
+//     let response_object = {}
+//     if (typeof response.data === "string") {
+//       console.log('Cleaned ECG. Response is:', response.data)
+//       response_object = JSON.parse(response.data.replace(/\bNaN\b/g, -1))
+//     } else {
+//       response_object = response.data
+//     }
+//
+//
+//     if (response_object.statusCode === 200) {
+//       all_data.push(...response_object.data.clean_ecg)
+//       request.object.set('ECG', all_data);
+//       const current_bpm = request.object.get('bpm');
+//       current_bpm.push(response_object.data.bpm);
+//       request.object.set('bpm', current_bpm);
+//       request.object.set('raw_ECG', []);
+//     }
+//   }
+// })
 
 Parse.Cloud.define('predict', async (req) => {
   try {
